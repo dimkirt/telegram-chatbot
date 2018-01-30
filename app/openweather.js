6461aae2a46a4fa5f6dc2634a3effd0502d01be6
Dimitrios Kirtsios <dimkirts@gmail.com>
2018-01-30 19:36:05 +0200

    Change weather response

--- app/openweather.js
+++ app/openweather.js
@@ -6,14 +6,14 @@ const fetch = require('node-fetch');
 const telegram = require('./telegram');
 
 function sendWeather(chatId, city, country){
-    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${weatherToken}&lang=el&units=metric`)
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${weatherToken}&lang=en&units=metric`)
         .then(res => res.json())
         .then(res => {
             //console.log(res);  // for debugging
             const weatherMsg = `<strong>${res.name}: </strong> ${res.weather[0].description}
-            <strong>Θερμοκρασία: </strong>${res.main.temp}°C,  
-            <strong>Υγρασία: </strong>${res.main.humidity}%, 
-            <strong>Άνεμοι: </strong>${res.wind.speed} μποφόρ`;
+            <strong>Temperature: </strong>${res.main.temp}°C,  
+            <strong>Humidity: </strong>${res.main.humidity}%, 
+            <strong>Winds: </strong>${res.wind.speed} Bft`;
 
             telegram.sendMessageHTML(chatId, weatherMsg);
         })
