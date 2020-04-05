740e0836c8af3a619a0d8ec7222562ea9be4072e
Dimitrios Kirtsios <dimkirts@gmail.com>
2020-04-05 21:29:14 +0200

    refactor: Remove unsupported reactions

--- src/app/reactions.js
+++ src/app/reactions.js
@@ -1,101 +1,70 @@
-'use strict';
+const greekUtils = require('greek-utils');
+const { TelegramBotApi } = require('../libs/telegram');
 
-const imgur = require('./imgur');
-const telegram = require('./telegram');
-const weather = require('./openweather');
-const news = require('./newsapi');
-const utils = require('./utils');
+const telegramBotApi = new TelegramBotApi({
+  apiToken: process.env.TELEGRAM_BOT_API_TOKEN,
+});
 
-
-// ---------------- CUSTOM FUNCTIONS
-function sendLands(chatId){
-    return imgur.getRandomLandscape()
-        .then(imgUrl => telegram.sendPicture(chatId, imgUrl))
-        .catch(err => console.log(err));
-}
 
 module.exports = {
-    reactToUserMessage: function(chatId, user, botMessage, text, msgId){
-        // REACT ONLY WITH TEXT
-        text = utils.standardizeString(text);
-        if(text === 'pop'){
-            if(user === 'dimkirt'){
-                return telegram.sendMessage(chatId, 'Ναι μπαμπά;');
-            }
-            return telegram.sendMessage(chatId, 'NETI');
-        }
+  reactToUserMessage(message) {
+    const { text } = message;
+    const chatId = message.chat.id;
+    const user = message.from.username;
+    const msgId = message.message_id;
 
-        else if(text === 'end_conversation'){
-            return telegram.sendMessage(chatId, 'Ante Geiaaaaaaaaaa!');
-        }
+    const standardizedText = greekUtils.toGreeklish(text).toLowerCase();
 
-        // Reply
-        else if(text.includes('popira')){
-            return telegram.replyWithMessage(chatId, 'parakalw', msgId);
-        }
-
-        // REACT ONLY WITH PICTURE
-        else if(text.includes('dab')){
-            return telegram.sendPicture(chatId, 'https://imgur.com/bsiD2P7');
-        }
-
-        else if(text.includes('sad')){
-            const caption = 'ＳＡＤ　ＢＯＹＺ';
-            return telegram.sendPictureCaptioned(chatId, 'https://imgur.com/LeyYNEa', caption);            
-        }
-
-        // REACT ONLY WITH GIF
-        else if(text.includes('meli')){
-            return telegram.sendDocument(chatId, 'https://imgur.com/0K9r9n0.gif');
-        }
-
-        // REACT WITH TEXT AND PICTURE
-        else if(text === 'pop koumpwneis?'){
-            const p1 = telegram.sendMessage(chatId, 'Όχι μαν μου, μόνο φυτικά παίρνω');
-            const p2 = telegram.sendPicture(chatId, 'https://imgur.com/ek277iv');
-            return Promise.all([p1, p2]);
-        }
-    
-        // REACT WITH TEXT AND GIF
-        else if(text === 'pop eisai kala?'){
-            const p1 = telegram.sendMessage(chatId, 'Όχι μαν μου');
-            const p2 = telegram.sendDocument(chatId, 'https://imgur.com/FWlJnDD.gif');
-            return Promise.all([p1, p2]);
-        }
-
-        // CUSTOM REACTIONS
-        else if(text === 'news_google'){
-            return news.sendNews(chatId, 'google-news');
-        }
-
-        else if(text === 'weather_thessaloniki'){
-            return weather.sendWeather(chatId, 'Thessaloniki', 'gr');
-        }
-
-        else if(text === 'weather_munich'){
-            return weather.sendWeather(chatId, 'Munich', 'de');
-        }
-
-        else if(text === 'images_nature'){
-            return sendLands(chatId);
-        }
-
-        else if(text === 'playlist_hiphop'){
-            return telegram.sendMessage(chatId, 'https://www.youtube.com/playlist?list=PLAFQAQCf660pJgBxh13wGlbK_aQDoFZLi');
-        }
-
-        else if(text === 'location_dps'){
-            return telegram.sendLocation(chatId, 48.176464, 11.592553);
-        }
-
-        else{
-            const promisesArr = botMessage.map(x => telegram.sendMessage(chatId, x));
-            return promisesArr.reduce((accum, task) => {
-                return Promise.all([accum, task])
-                    .then(res => [... res[0], res[1]]);                
-            }, Promise.resolve([]));
-            
-            //return telegram.sendMessage(chatId, botMessage[0]);
-        }
+    if (standardizedText === 'pop') {
+      if (user === 'dimkirt') {
+        return telegramBotApi.sendMessage(chatId, 'Ναι μπαμπά;');
+      }
+      return telegramBotApi.sendMessage(chatId, 'NETI');
     }
+
+    if (standardizedText === 'end_conversation') {
+      return telegramBotApi.sendMessage(chatId, 'Ante Geiaaaaaaaaaa!');
+    }
+
+    if (standardizedText.includes('popira')) {
+      return telegramBotApi.sendMessage(chatId, 'parakalw', { reply_to_message_id: msgId });
+    }
+
+    if (standardizedText.includes('dab')) {
+      return telegramBotApi.sendPhoto(chatId, 'https://imgur.com/bsiD2P7');
+    }
+
+    if (standardizedText.includes('sad')) {
+      const caption = 'ＳＡＤ　ＢＯＹＺ';
+      return telegramBotApi.sendPhoto(chatId, 'https://imgur.com/LeyYNEa', { caption });
+    }
+
+    if (standardizedText.includes('meli')) {
+      return telegramBotApi.sendDocument(chatId, 'https://imgur.com/0K9r9n0.gif');
+    }
+
+    if (standardizedText === 'pop koumpwneis?') {
+      const p1 = telegramBotApi.sendMessage(chatId, 'Όχι μαν μου, μόνο φυτικά παίρνω');
+      const p2 = telegramBotApi.sendPhoto(chatId, 'https://imgur.com/ek277iv');
+      return Promise.all([p1, p2]);
+    }
+
+    if (standardizedText === 'pop eisai kala?') {
+      const p1 = telegramBotApi.sendMessage(chatId, 'Όχι μαν μου');
+      const p2 = telegramBotApi.sendDocument(chatId, 'https://imgur.com/FWlJnDD.gif');
+      return Promise.all([p1, p2]);
+    }
+
+    if (standardizedText === 'playlist_hiphop') {
+      return telegramBotApi.sendMessage(
+        chatId, 'https://www.youtube.com/playlist?list=PLAFQAQCf660pJgBxh13wGlbK_aQDoFZLi',
+      );
+    }
+
+    if (standardizedText === 'location_dps') {
+      return telegramBotApi.sendLocation(chatId, { latitude: 48.176464, longitude: 11.592553 });
+    }
+
+    return Promise.resolve();
+  },
 };
