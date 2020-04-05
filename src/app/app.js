1688a0fa79162a79efad941292b997aa687f2644
Dimitrios Kirtsios <dimkirts@gmail.com>
2020-04-05 21:16:25 +0200

    refactor: Split index.js file
    We construct and bootstrap the app in separate files.

---
+++ src/app/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const reactions = require('./reactions');
+
+function cors(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+  next();
+}
+
+const asyncErrorHandler = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
+
+function createApp() {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(cors);
+
+  app.post('/messages', asyncErrorHandler(async (req, res) => {
+    const message = req.body.message ? req.body.message : req.body.edited_message;
+    await reactions.reactToUserMessage(message);
+    return res.json({ success: true });
+  }));
+
+  app.get('/health', (req, res) => res.json({ up: true }));
+
+  return app;
+}
+
+module.exports = {
+  createApp,
+};
