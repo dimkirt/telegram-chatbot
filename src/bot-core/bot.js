4e111795838fc3372ea59dc7d2b4a75ba1a74656
Dimitrios Kirtsios <dimkirts@gmail.com>
2020-04-12 15:29:38 +0200

    refactor: Extract actions service to own module

--- src/bot-core/bot.js
+++ src/bot-core/bot.js
@@ -1,94 +1,10 @@
 class Bot {
   constructor({
-    telegramBotApi,
-    openWeatherApi,
-    newsApi,
-    imgurApi,
-    actionRepository,
+    actionService,
     intentionService,
   }) {
-    this.telegramBotApi = telegramBotApi;
-    this.openWeatherApi = openWeatherApi;
-    this.newsApi = newsApi;
-    this.imgurApi = imgurApi;
-    this.actionRepository = actionRepository;
+    this.actionService = actionService;
     this.intentionService = intentionService;
-    this.functionFactory = {
-      sendMessage: this.sendMessage,
-      sendPhoto: this.sendPhoto,
-      sendDocument: this.sendDocument,
-      sendLocation: this.sendLocation,
-      sendMessageWithWeatherForCity: this.sendMessageWithWeatherForCity,
-      sendMessageArticlesForNewsSource: this.sendMessageArticlesForNewsSource,
-    };
-  }
-
-  executeAction({ action, chatId, msgId }) {
-    const isValidAction = action.type in this.functionFactory;
-    if (isValidAction) {
-      const payload = { ...action.payload, chatId, msgId };
-      return this.functionFactory[action.type].call(this, payload);
-    }
-    return Promise.resolve();
-  }
-
-  sendMessage(payload) {
-    const options = {};
-    if (payload.message.reply) {
-      options.reply_to_message_id = payload.msgId;
-    }
-    return this.telegramBotApi.sendMessage(payload.chatId, payload.message.text, options);
-  }
-
-  sendPhoto(payload) {
-    const options = {};
-    if (payload.photo.caption) {
-      options.caption = payload.photo.caption;
-    }
-    return this.telegramBotApi.sendPhoto(payload.chatId, payload.photo.photoUrl, options);
-  }
-
-  sendDocument(payload) {
-    const options = {};
-    if (payload.document.caption) {
-      options.caption = payload.document.caption;
-    }
-    return this.telegramBotApi.sendDocument(payload.chatId, payload.document.documentUrl, options);
-  }
-
-  sendLocation(payload) {
-    const options = {};
-    if (payload.location.caption) {
-      options.caption = payload.location.caption;
-    }
-    return this.telegramBotApi.sendLocation(payload.chatId, payload.location, options);
-  }
-
-  async sendMessageWithWeatherForCity(payload) {
-    const weatherData = await this.openWeatherApi.getWeatherByCity(payload.city, payload.country);
-    const text = `<strong>${weatherData.name}: </strong> ${weatherData.weather[0].description}
-      <strong>Temperature: </strong>${weatherData.main.temp}°C,  
-      <strong>Humidity: </strong>${weatherData.main.humidity}%, 
-      <strong>Winds: </strong>${weatherData.wind.speed} Bft`;
-
-    const options = {
-      parse_mode: 'HTML',
-    };
-
-    return this.telegramBotApi.sendMessage(payload.chatId, text, options);
-  }
-
-  async sendMessageArticlesForNewsSource(payload) {
-    function sendNewsArticleToTelegram(article) {
-      const options = {
-        parse_mode: 'HTML',
-      };
-      const text = `<b>${article.title}:</b> ${article.url}`;
-      return this.telegramBotApi.sendMessage(payload.chatId, text, options);
-    }
-
-    const articles = await this.newsApi.getArticlesBySource(payload.source);
-    return Promise.all(articles.map(sendNewsArticleToTelegram));
   }
 
   async reactToUserMessage(message) {
@@ -99,18 +15,12 @@ class Bot {
       user: message.from.username,
     };
     const actionId = this.intentionService.determineAction(determineActionDto);
-    const action = await this.actionRepository.findOne({ id: actionId });
-
-    if (!action) {
-      return Promise.resolve();
-    }
-
     const executeActionDto = {
-      action,
+      actionId,
       chatId: message.chat.id,
       msgId: message.message_id,
     };
-    return this.executeAction(executeActionDto);
+    return this.actionService.executeAction(executeActionDto);
   }
 }
 
--- src/bot-core/index.js
+++ src/bot-core/index.js
@@ -1,36 +1,12 @@
-
-const { TelegramBotApi } = require('../libs/telegram');
-const { OpenWeatherApi } = require('../libs/open-weather-map');
-const { NewsApi } = require('../libs/news-api');
-const { ImgurApi } = require('../libs/imgur');
-
 const { Bot } = require('./bot');
 const { createIntentionService } = require('./intentions');
-const { ActionRepository } = require('./action-repository');
+const { createActionService } = require('./actions');
 
 function createBot() {
-  const telegramBotApi = new TelegramBotApi({
-    apiToken: process.env.TELEGRAM_BOT_API_TOKEN,
-  });
-
-  const openWeatherApi = new OpenWeatherApi({
-    apiToken: process.env.OPENWEATHER_API_TOKEN,
-    language: 'en',
-  });
-
-  const newsApi = new NewsApi({
-    apiToken: process.env.NEWS_API_TOKEN,
-  });
-
-  const imgurApi = new ImgurApi({
-    apiClientId: process.env.IMGUR_CLIENT_ID,
-    apiClientSecret: process.env.IMGUR_CLIENT_SECRET,
-  });
-
   const intentionService = createIntentionService();
-  const actionRepository = new ActionRepository();
+  const actionService = createActionService();
   return new Bot({
-    telegramBotApi, openWeatherApi, newsApi, imgurApi, actionRepository, intentionService,
+    actionService, intentionService,
   });
 }
 
