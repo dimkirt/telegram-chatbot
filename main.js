783f1cf307fde7ac23de789c392fef8f5f9d2391
Dimitrios Kirtsios <dimkirts@gmail.com>
2020-04-12 14:42:29 +0200

    fix: Fix wrong import

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { TelegramBotApi } = require('./src/libs/telegram');
-const { createApp } = require('./src/app/app.js');
+const { createApp } = require('./src/rest-api/app.js');
 
 const DEFAULT_PORT = 3000;
 
